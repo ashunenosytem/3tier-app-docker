b768498f33183d93200b371315565ce97dac7b98
ashunenosytem <ashutosh.ahirwar@nenosystems.in>
2026-02-28 18:24:16 +0530

    s3 state locking added

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "ansible-playbook-back"
-    key            = "test/terraform.tfstate"
+    key            = "t1/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
+    use_lockfile   = true
     encrypt        = true
   }
 }
