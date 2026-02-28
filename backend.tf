50e7a861405236fcbd4354544cbfe1bf09968d5d
ashunenosytem <ashutosh.ahirwar@nenosystems.in>
2026-02-28 18:50:11 +0530

    s3 state locking added

--- backend.tf
+++ backend.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = "1.14.6"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      # Using v6.x for the latest features and India (ap-south-1) optimizations
-      version = "~> 6.0"
-    }
-  }
   backend "s3" {
     bucket         = "ansible-playbook-back"
     key            = "t1/terraform.tfstate"
